b3e40a089f18ccf238553e144b4367c0393bd50c
Jakub Sumara <jakub.sumara@minebest.com>
2021-01-24 15:39:22 +0100

    Init react

---
+++ view/src/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <React.StrictMode>
+      <div>
+          Test
+      </div>
+  </React.StrictMode>,
+  rootElement
+);
+
