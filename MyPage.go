1b30fcd64000f49c063b3d6d1f3e170973ae1888
Jakub Sumara <jakub.sumara@minebest.com>
2021-01-25 22:02:55 +0100

    Created simple message crud

--- MyPage.go
+++ MyPage.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "hello\n")
+type Message struct {
+	gorm.Model
+	Message string
+}
+
+var db, _ = gorm.Open(mysql.Open("user:pass@tcp(127.0.0.1:3306)/mypage?charset=utf8&parseTime=True"), &gorm.Config{})
+
+func getMessages(w http.ResponseWriter, r *http.Request)  {
+	var m []Message
+	db.Find(&m)
+	json.NewEncoder(w).Encode(&m)
+}
+
+func sendMessage(w http.ResponseWriter, r *http.Request)  {
+	var m Message
+	json.NewDecoder(r.Body).Decode(&m)
+	db.Create(&m)
 }
 
 func main() {
-	http.HandleFunc("/", home)
+	r := mux.NewRouter().StrictSlash(true)
+	r.HandleFunc("/messages", getMessages).Methods("GET")
+	r.HandleFunc("/message", sendMessage).Methods("POST")
 
-	http.ListenAndServe(":8090", nil)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
